398968ea2a54b6f919c5af04506981e0176f1b6b
Isaak Maisis <143521058+zakzak4@users.noreply.github.com>
2026-04-20 15:56:12 +0300

    Added folder and updated file

--- admin/config.php
+++ admin/config.php
@@ -13,7 +13,7 @@ define('HTTPS_CATALOG', 'http://localhost/shop/');
 define('DIR_APPLICATION', 'C:/xampp/htdocs/shop/admin/');
 define('DIR_SYSTEM', 'C:/xampp/htdocs/shop/system/');
 define('DIR_IMAGE', 'C:/xampp/htdocs/shop/image/');
-define('DIR_STORAGE', 'C:/xampp/storage/');
+define('DIR_STORAGE', 'C:/xampp/htdocs/shop/storage/');
 define('DIR_CATALOG', 'C:/xampp/htdocs/shop/catalog/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
--- config.php
+++ config.php
@@ -11,7 +11,7 @@ define('HTTPS_SERVER', 'http://localhost/shop/');
 define('DIR_APPLICATION', 'C:/xampp/htdocs/shop/catalog/');
 define('DIR_SYSTEM', 'C:/xampp/htdocs/shop/system/');
 define('DIR_IMAGE', 'C:/xampp/htdocs/shop/image/');
-define('DIR_STORAGE', 'C:/xampp/storage/');
+define('DIR_STORAGE', 'C:/xampp/htdocs/shop/storage/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
 define('DIR_CONFIG', DIR_SYSTEM . 'config/');
